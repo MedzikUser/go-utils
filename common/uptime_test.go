bd19c5461045068dbbbb20e6f45612b87ceddddd
Oskar <nivua1fn@duck.com>
2021-10-24 14:36:47 +0200

    Update uptime_test.go

--- common/uptime_test.go
+++ common/uptime_test.go
@@ -6,5 +6,6 @@ import (
 )
 
 func TestCheckUptime(T *testing.T) {
-	Uptime(time.Date(2016, 6, 2, 1, 1, 1, 1, time.UTC))
+	uptime := Uptime(time.Date(2016, 6, 2, 1, 1, 1, 1, time.UTC))
+fmt.Println(uptime)
 }
